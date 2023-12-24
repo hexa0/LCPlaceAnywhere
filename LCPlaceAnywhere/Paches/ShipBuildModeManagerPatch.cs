0ccd15683ff3db1320c2dfcad9bdeb0d836498fe
hexa0 <67880600+hexa0@users.noreply.github.com>
2023-12-24 00:16:55 -0500

    add LCPlaceAnywhere

---
+++ LCPlaceAnywhere/Paches/ShipBuildModeManagerPatch.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+
+namespace LCPlaceAnywhere.Patches
+{
+    [HarmonyPatch(typeof(ShipBuildModeManager))]
+    internal class ShipBuildModeManagerPatch
+    {
+        [HarmonyPatch("EnterBuildMode")]
+        [HarmonyPrefix]
+        static void forceAllowBuildPatch(ref float ___timeSincePlacingObject, ref bool ___CanConfirmPosition)
+        {
+            ___timeSincePlacingObject = 1.69420f;
+            ___CanConfirmPosition = true;
+        }
+
+        [HarmonyPatch("PlayerMeetsConditionsToBuild")]
+        [HarmonyPostfix]
+        static void forceAllowBuildOutsideShip(ref bool __result)
+        {
+            __result = true;
+        }
+    }
+}
---
+++ LCPlaceAnywhere/Plugin.cs
@@ -0,0 +1,38 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using LCPlaceAnywhere.Patches;
+
+namespace LCPlaceAnywhere
+{
+    [BepInPlugin(PLUGIN_GUID, PLUGIN_NAME, PLUGIN_VERSION)]
+    public class LCPlaceAnywhereBase : BaseUnityPlugin
+    {
+        private const string PLUGIN_GUID = "Hexa.LCPlaceAnywhere";
+        private const string PLUGIN_NAME = "LCPlaceAnywhere";
+        private const string PLUGIN_VERSION = "1.0.0.0";
+
+        private readonly Harmony harmony = new Harmony(PLUGIN_GUID);
+
+        private static LCPlaceAnywhereBase Instance;
+
+        internal ManualLogSource mls;
+
+
+        private void Awake()
+        {
+            // Plugin startup logic
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            mls = BepInEx.Logging.Logger.CreateLogSource(PLUGIN_GUID);
+
+            mls.LogInfo($"Plugin {PLUGIN_GUID} is loaded!");
+            harmony.PatchAll(typeof(LCPlaceAnywhereBase));
+            harmony.PatchAll(typeof(ShipBuildModeManagerPatch));
+            harmony.PatchAll(typeof(HangarShipDoorPatch));
+        }
+    }
+}
